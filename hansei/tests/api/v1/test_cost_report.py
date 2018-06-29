955978c29367c4f345e9d1e8a757989cf8987744
Nandini Chandra <nachandr@redhat.com>
2018-06-29 14:26:05 -0500

    Tests for cost report API

--- hansei/tests/api/v1/test_cost_report.py
+++ hansei/tests/api/v1/test_cost_report.py
@@ -22,11 +22,12 @@ def test_validate_totalcost():
                             'password': 'str0ng!P@ss', 'email': 'foo@bar.com'})
     customer.login()
     report = KokuCostReport(customer.client)
+    report.get()
 
     # Fetch daily costs and sum up the values
-    for d in report.get.data:
-        sum = sum + d['total_cost'] if d['total_cost'] else 0.0
+    for d in report.data:
+        sum = sum + (d['total_cost'] if d['total_cost'] else 0.0)
 
-    assert report.total['value'] - DEVIATION == sum <= \
+    assert report.total['value'] - DEVIATION <= sum <= \
         report.total['value'] + DEVIATION, \
         'Report total is not equal to the sum of daily costs'
